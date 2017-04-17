51e8dd71585ef6228f6311b0324579aae953bcd8
alltazar <da_nil@ukr.net>
2017-04-17 23:54:34 +0300

    Update binarySearch.java

--- src/danil/binarySearch.java
+++ src/danil/binarySearch.java
@@ -4,6 +4,19 @@ public class BinarySearch {
         int end = a.length -1;
 
         int c = -1;
+        if (a.length == 0){
+            return c;
+        } else if (a.length <= 2){
+            if (a[0] == b){
+                c = 0;
+                return c;
+            } else if (a[1] == b){
+                c = 1;
+                return c;
+            } else {
+                return c;
+            }
+        }
 
         while (start!=end){
             if (a[(end+start)/2] == b){
