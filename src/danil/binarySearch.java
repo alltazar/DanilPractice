ecb3193649a4aebff547f3922c93ed38bb7bbaed
alltazar <da_nil@ukr.net>
2017-04-17 23:23:43 +0300

    Update binarySearch.java

--- src/danil/binarySearch.java
+++ src/danil/binarySearch.java
@@ -1,30 +1,18 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int[] arr = {5, 12, 22, 23, 26, 31, 35, 46, 51, 56};
-        int x = 56;
-
-        System.out.println(binarySearch(arr, x));
-    }
-
+public class BinarySearch {
     public static int binarySearch(int[] a, int b){
         int start = 0;
         int end = a.length -1;
-        int middle = (end-start)/2;
 
-        int c = 0;
+        int c = -1;
 
         while (start!=end){
             if (a[(end+start)/2] == b){
                 c = (end+start)/2;
                 break;
-            } else if (a[end] == b) {
-                c = end;
-                break;
             } else if (a[(end+start)/2] > b) {
-                end = (end+start)/2;
+                end = ((end+start)/2)-1;
             } else {
-                start = (end+start)/2;
+                start = ((end+start)/2)+1;
             }
         }
 
